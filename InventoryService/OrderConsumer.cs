469c60cffbd8820f5bccafec4a603a7effe4e9d3
lohith <lohithmys86@gmail.com>
2022-04-28 09:10:58 +0530

    Add project files.

---
+++ InventoryService/OrderConsumer.cs
@@ -0,0 +1,14 @@
+﻿using InventoryService.Models;
+using MassTransit;
+
+namespace InventoryService
+{
+    public class OrderConsumer:IConsumer<Order>
+    {
+        public async Task Consume(ConsumeContext<Order> context)
+        {
+            var msg = context.Message;
+            await Console.Out.WriteLineAsync(msg.Details);
+        }
+    }
+}
---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+        public OrderController(IPublishEndpoint PublishEndpoint)
+        {
+            this._publishEndpoint = PublishEndpoint;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order)
+        {
+            await _publishEndpoint.Publish<Order>(order);
+            return Ok();
+        }  
+    }
+}
